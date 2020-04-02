77989d167518174a8d9c326bd47de3e56127a0a7
Jacob Heard <Jacob.Heard@D2L.com>
2020-04-02 15:11:41 -0400

    Add output_dir argument to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-	echo Usage: $0 docs_path
+if [ $# -ne 2 ]; then
+	echo Usage: $0 docs_path output_dir
 	exit 1
 fi
 
-dotnet run -p /render/src/Render/D2L.Dev.Docs.Render.csproj --input $1 --output output
+dotnet run -p /render/src/Render/D2L.Dev.Docs.Render.csproj --input $1 --output $2
