89e99b8b21e4788bc051e5c362c096ac96db6841
Jacob Heard <Jacob.Heard@D2L.com>
2020-04-17 12:02:52 -0400

    use FileInfo for RelativeFile

--- src/Render/RelativeFile.cs
+++ src/Render/RelativeFile.cs
@@ -1,33 +1,35 @@
-﻿using System.IO;
+﻿using D2L.Dev.Docs.Render.Markdown;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace D2L.Dev.Docs.Render {
+
 	internal sealed class RelativeFile {
 
-		public string Path { get; }
-		public string SourceRoot { get; }
-		public string DestinationRoot { get; }
-		public string Name { get { return System.IO.Path.GetFileNameWithoutExtension( Path ); } }
-		public string Extension { get { return System.IO.Path.GetExtension( Path ).TrimStart( '.' ); } }
+		public FileInfo Source { get; }
+		public FileInfo Destination { get; }
+		public DocumentContext Context { get; }
 
-		public RelativeFile( string srcRoot, string dstRoot, string path ) {
-			SourceRoot = srcRoot;
-			DestinationRoot = dstRoot;
-			Path = path;
+		public RelativeFile( DocumentContext context, string path, string dstName = null ) {
+			Context = context;
+			Source = new FileInfo( path );
+
+			string relparent = Path.GetRelativePath( Context.InputDirectory, Source.Path );
+			string dstpath = Path.Join( Context.OutputDirectory, relparent, dstName );
+			Destination = new FileInfo( dstpath );
 		}
 
 		public async Task<string> Read() {
-			return await File.ReadAllTextAsync( Path );
+			return await File.ReadAllTextAsync( Source.FullPath );
 		}
 
 		/// <summary>
 		/// Copies source file to destination file
 		/// </summary>
 		/// <param name="newName">new filename to use for destination, defaults to same name as source</param>
-		public void Copy( string newName = null ) {
-			var path = GetDestinationPath( newName );
-			GuaranteeParent( path );
-			File.Copy( Path, path );
+		public void Copy() {
+			new DirectoryInfo( Destination.Path ).Create();
+			File.Copy( Source.FullPath, Destination.FullPath );
 		}
 
 		/// <summary>
@@ -36,21 +38,9 @@ namespace D2L.Dev.Docs.Render {
 		/// <param name="data">data to write to the file</param>
 		/// <param name="newName">new filename to use for destination, defaults to same name as source</param>
 		/// <returns></returns>
-		public async Task Write( string data, string newName = null ) {
-			var path = GetDestinationPath( newName );
-			GuaranteeParent( path );
-			await File.WriteAllTextAsync( path, data );
-		}
-
-		private string GetDestinationPath( string newName = null ) {
-			newName = newName ?? System.IO.Path.GetFileName( Path );
-			string parent = Directory.GetParent( Path ).FullName;
-			string relparent = System.IO.Path.GetRelativePath( SourceRoot, parent );
-			return System.IO.Path.Join( DestinationRoot, relparent, newName );
-		}
-
-		private void GuaranteeParent( string path ) {
-			Directory.GetParent( path ).Create();
+		public async Task Write( string data ) {
+			new DirectoryInfo( Destination.Path ).Create();
+			await File.WriteAllTextAsync( Destination.FullPath, data );
 		}
 
 	}
