2a900e1bfc3775f69c1f99e361f10d1ccd286a4c
Jacob Heard <Jacob.Heard@D2L.com>
2020-04-17 11:50:32 -0400

    Added FileInfo struct

---
+++ src/Render/FileInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace D2L.Dev.Docs.Render {
+	internal readonly struct FileInfo {
+		public string Path { get; }
+		public string Name { get; }
+		public string Extension { get; }
+		public string FullPath {
+			get {
+				return System.IO.Path.Join( Path, $"{Name}.{Extension}" );
+			}
+		}
+
+		public FileInfo( string path ) {
+			Path = path.Substring( 0, path.LastIndexOf( '/' ) );
+			Name = System.IO.Path.GetFileNameWithoutExtension( path );
+			Extension = System.IO.Path.GetExtension( path ).TrimStart( '.' );
+		}
+		public FileInfo( string path, string name, string extension ) {
+			Path = path;
+			Name = name;
+			Extension = extension;
+		}
+	}
+}
