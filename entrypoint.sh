e8335ec9f6c624b8c39c57006678187f347fa4d8
Jacob Parker <jacob@solidangle.ca>
2021-12-17 09:36:56 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo Usage: $0 docs_path output_dir
-	exit 1
-fi
-
 cd /render/src/Render
 
-dotnet run --no-launch-profile -p D2L.Dev.Docs.Render.csproj --repo-root /github/workspace/ --docs-path $1 --output /github/workspace/$2
+if [ ! -z $3 ]; then
+	dotnet run --no-launch-profile -p D2L.Dev.Docs.Render.csproj --repo-root /github/workspace/ --docs-path $1 --output /github/workspace/$2 --template-path $3
+else
+	dotnet run --no-launch-profile -p D2L.Dev.Docs.Render.csproj --repo-root /github/workspace/ --docs-path $1 --output /github/workspace/$2
+fi
