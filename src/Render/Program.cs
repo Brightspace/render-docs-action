7d7560dbfc3f4b16ffed1c2bd52099b3948ecf18
Jacob Parker <jacob@solidangle.ca>
2021-12-17 09:07:46 -0500

    Fix stuff I accidentally stomped

--- src/Render/Program.cs
+++ src/Render/Program.cs
@@ -9,17 +9,22 @@ using D2L.Dev.Docs.Render.VFS;
 
 namespace D2L.Dev.Docs.Render {
 	internal static class Program {
-		/// <param name="input">The path to a directory of markdown files to render</param>
-		/// <param name="output">The directory to put the rendered html files</param>
+		/// <param name="repoRoot">The path to repo root directory.</param>
+		/// <param name="output">The directory to put the rendered html files.</param>
+		/// <param name="docsPath">The path within the repo containing the docs files to be rendered.</param>
+		/// <param name="templatePath">An optional path to a template.</param>
 		public static async Task<int> Main(
-			string input,
+			string repoRoot,
 			string output,
+			string docsPath,
 			string templatePath = null
 		) {
-			if ( input == null || output == null ) {
-				Console.Error.WriteLine( "--input and --output arguments are required" );
+			if ( repoRoot == null || output == null || docsPath == null ) {
+				Console.Error.WriteLine( "--repo-root, --output, and --docs-path arguments are required" );
 				return -1;
 			}
+
+			string input = Path.Combine( repoRoot, docsPath );
 
 			if( !Directory.Exists( input ) ) {
 				Console.Error.WriteLine( "input must be a  directory containing markdown (.md) files" );
@@ -29,6 +34,8 @@ namespace D2L.Dev.Docs.Render {
 			if( !Directory.Exists( output ) ) {
 				Directory.CreateDirectory( output );
 			}
+
+			string docsPathSanitized = GetSanitizedDocsPath( docsPath, GetBranch(), docsPathSanitized );
 
 			// See https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables
 			var repoName = Environment.GetEnvironmentVariable( "GITHUB_REPOSITORY" )?.Split( '/' )[1] ?? "";
@@ -40,6 +47,34 @@ namespace D2L.Dev.Docs.Render {
 			}
 
 			return 0;
+		}
+
+		private static string GetSanitizedDocsPath( string docsPath ) {
+			string docsPathSanitized;
+			if( docsPath == "." ) {
+				docsPathSanitized = "";
+			} else {
+				// Remove leading slashes and ensure there's only one trailing slash
+				docsPathSanitized = docsPath.TrimStart( '/' ).TrimEnd( '/' ) + '/';
+			}
+
+			return docsPathSanitized;
+		}
+
+		private static string GetBranch() {
+			// See https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables
+			var gitRef = Environment.GetEnvironmentVariable( "GITHUB_REF" );
+
+			return GetBranchFromRef( gitRef );
+		}
+
+		internal static string GetBranchFromRef( string? gitRef ) {
+			// Currently only supports main or master branches.
+			// Want to support arbitrary default branches, but starting lazy.
+			return gitRef switch {
+				"refs/heads/main" => "main",
+				_ => "master",
+			};
 		}
 
 		private static async Task DoFile( DocumentContext context, RelativeFile file, string templatePath ) {
@@ -150,7 +185,7 @@ namespace D2L.Dev.Docs.Render {
 
 			string relativePath = Path.GetRelativePath( context.InputDirectory, path );
 			Uri editSourceUri = new Uri(
-				$"https://github.dev/Brightspace/{context.DocRootRepoName}/master/{relativePath}",
+				$"https://github.com/Brightspace/{context.DocRootRepoName}/edit/{context.Branch}/{context.DocsPath}{relativePath}",
 				UriKind.Absolute
 			);
 
